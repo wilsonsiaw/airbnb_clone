a0cd5ec15dc158f64ba35d37e372ffbecae397b3
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-15 15:54:23 +0200

    ✨ feat(Navbar Component): Create component
    Create the Navbar and add the content of the navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css';
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="airbnb page logo red" />
+    </nav>
+  )
+}
+
+export default Navbar
