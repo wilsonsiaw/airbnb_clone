12b5309ad6b0c688e24d7434d3b664083023f914
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-15 16:47:48 +0200

    ✨ feat(Application Component): Add Hero
    Add the Hero Component the the root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
