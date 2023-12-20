dacb0837efa2d01d2ef7287e470f0dbe1624bbab
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-20 14:08:59 +0200

    ✨ feat(Application Component): Add Card
    Add the Card Component to the root component to be rendered

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Card from './components/Card/Card'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
