3b761300fb9dd7ee60c8687bbe4fae660d24eb87
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-20 14:12:40 +0200

    ✨ feat(Card Component): Create Card
    Create a Card Component and the necessary content to it

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Card.css'
+import photo from '../../assets/katie-zaferes.png'
+import star from '../../assets/star.png'
+
+const Card = () => {
+  return (
+    <section className='cardWrapper'>
+      <div className='photo'>
+        <img src={photo} alt="A photo of Katie Zaferes smiling" />
+        <h4>SOLD OUT</h4>
+      </div>
+      <div className='cardContent'>
+        <div className='rating'>
+            <img src={star} alt="A red ratings star" />
+            <p>5.0</p>
+            <p>(6)</p>
+            <p>USA</p>
+        </div>
+        <p id='para1'>Life lessons with Katie Zaferes</p>
+        <p id='para2'>From $136 / <span>person</span></p>
+      </div>
+    </section>
+  )
+}
+
+export default Card
