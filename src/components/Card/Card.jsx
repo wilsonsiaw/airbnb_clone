b58cbd35c1b2c0bcb4294f3dd5da5bea6da6593e
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-25 13:15:09 +0200

    🦄 refactor(Card Component): Introduce Props
    Introduce props to make the component more data driven and less reliant on hard coded data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import './Card.css'
-import photo from '../../assets/katie-zaferes.png'
+// import photo from '../../assets/katie-zaferes.png'
 import star from '../../assets/star.png'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <section className='cardWrapper'>
       <div className='photo'>
         <div className="soldOutOverlay">
           <h4>SOLD OUT</h4>
         </div>
-        <img src={photo} alt="A photo of Katie Zaferes smiling" />
+        <img src={props.img} alt="A photo of Katie Zaferes smiling" />
       </div>
       <div className='cardContent'>
         <div className='rating'>
             <img src={star} alt="A red ratings star" />
-            <p>5.0</p>
-            <p>(6)</p>
-            <p>USA</p>
+            <p>{props.rating}</p>
+            <p>{props.reviewCount}</p>
+            <p>{props.country}</p>
         </div>
-        <p id='para1'>Life lessons with Katie Zaferes</p>
-        <p id='para2'>From $136 / <span>person</span></p>
+        <p id='para1'>{props.title}</p>
+        <p id='para2'>From {props.price} / <span>person</span></p>
       </div>
     </section>
   )
