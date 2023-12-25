8f58b1ae3cf0f018b0025e43ccd4a8b0ac4b544e
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-25 18:48:23 +0200

    🦄 refactor(Application Component): Add data
    Add data to the component and map over the data before rendering it

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Card from './components/Card/Card'
+import data from './data'
 
 function App() {
+
+  const cards = data.map((item) => 
+  <Card 
+    img={item.coverImg}
+    rating={item.stats.rating}
+    reviewCount={item.stats.reviewCount}
+    country={item.location}
+    title={item.title}
+    price={item.price}
+  />)
 
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card 
-        img="src/assets/katie-zaferes.png"
-        rating={5.0}
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   )
 }
