9f8146103ba801a7d1644bad84c9f8cb8efcb51c
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-31 13:57:30 +0200

    🦄 refactor(Application): Use Spread Syntax
    Use the spread syntax to spread the properties from the data file

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,7 @@ function App() {
   const cards = data.map((item) => 
   <Card
     key={item.id} 
-    img={item.coverImg}
-    rating={item.stats.rating}
-    reviewCount={item.stats.reviewCount}
-    country={item.location}
-    title={item.title}
-    price={item.price}
+    {...item}
   />)
 
   return (
