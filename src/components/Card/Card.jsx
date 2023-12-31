4670ccbded324ff8167e023ea6e66a4e5a3aeea2
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-31 13:59:00 +0200

    🦄 refactor(Card Component): Change Component
    Change the component by introducing conditional rendering for some of the part of the component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,14 +3,22 @@ import './Card.css'
 import star from '../../assets/star.png'
 
 const Card = (props) => {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "Online";
+  }
+
   return (
     <div className='card'>
-      <img src={props.img} className='card-image' />
+      {badgeText && <div className='card-badge'>{badgeText}</div>}
+      <img src={props.coverImg} className='card-image' />
       <div className='card-stats'>
           <img src={star} alt="A red ratings star" className='card-star'/>
-          <span>{props.rating}</span>
-          <span className='gray'>({props.reviewCount}) • </span>
-          <span className='gray'>{props.country}</span>
+          <span>{props.stats.rating}</span>
+          <span className='gray'>({props.stats.reviewCount}) • </span>
+          <span className='gray'>{props.location}</span>
       </div>
       <p className='card-title'>{props.title}</p>
       <p className='card-price'><span className='bold'>From {props.price}</span> / person</p>
